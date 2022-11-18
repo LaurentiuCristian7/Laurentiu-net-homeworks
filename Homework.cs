4ea7fba2569d0f5dee02231d52b75366281895d7
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2022-11-18 20:15:53 +0200

    Tema completa.

--- Homework.cs
+++ Homework.cs
@@ -76,66 +76,182 @@ namespace ConsoleApp1
             //Console.WriteLine("Minimum number: "+Math.Min(a, Math.Min(b, c)));
 
             //ex 5(---------------------Exercice5--------------)
-            
-           // Console.WriteLine("Physics Marks : ");
-           //  int mark1 = Convert.ToInt32(Console.ReadLine());
-           // Console.WriteLine(" Chemistry Marks : ");
-           //  int mark2 = Convert.ToInt32(Console.ReadLine());
-           // Console.WriteLine(" Biology Marks :");
+
+            // Console.WriteLine("Physics Marks : ");
+            //  int mark1 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(" Chemistry Marks : ");
+            //  int mark2 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(" Biology Marks :");
             // int mark3 = Convert.ToInt32(Console.ReadLine());
-           // Console.WriteLine("Mathematics Marks");
-           // int mark4= Convert.ToInt32(Console.ReadLine());
-           // Console.WriteLine(" Computer Science Marks");
-          //   int mark5= Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Mathematics Marks");
+            // int mark4= Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(" Computer Science Marks");
+            //   int mark5= Convert.ToInt32(Console.ReadLine());
 
-          //  int total = (mark1 + mark2 + mark3 + mark4 + mark5);
+            //  int total = (mark1 + mark2 + mark3 + mark4 + mark5);
 
-          //  float percentage = total/5f;
-
-
-           // Console.WriteLine("Total Marks : " + total);
-           // Console.WriteLine("Percentage : " + percentage);
-
-            
-           // if (percentage >= 90)
-           // {
-           //     Console.WriteLine("Grade is A");
-          //  }
-          //  else if ( percentage >=80)
-          //  {
-          //      Console.WriteLine("Grade is B");
-          //  }
-           // else if (percentage >= 70)
-          //  {
-          //      Console.WriteLine("Grade is C");
-          //  }
-          //  else if (percentage >= 60)
-           // {
-          //      Console.WriteLine("Grade is D");
-         //   }
-         //   else if (percentage>= 40)
-         //   {
-          //      Console.WriteLine("Grade is E");
-          //  }
-           // else if (percentage <40)
-          //  {
-          //      Console.WriteLine("Grade is F");
-          //  }
-            
+            //  float percentage = total/5f;
 
 
+            // Console.WriteLine("Total Marks : " + total);
+            // Console.WriteLine("Percentage : " + percentage);
+
+
+            // if (percentage >= 90)
+            // {
+            //     Console.WriteLine("Grade is A");
+            //  }
+            //  else if ( percentage >=80)
+            //  {
+            //      Console.WriteLine("Grade is B");
+            //  }
+            // else if (percentage >= 70)
+            //  {
+            //      Console.WriteLine("Grade is C");
+            //  }
+            //  else if (percentage >= 60)
+            // {
+            //      Console.WriteLine("Grade is D");
+            //   }
+            //   else if (percentage>= 40)
+            //   {
+            //      Console.WriteLine("Grade is E");
+            //  }
+            // else if (percentage <40)
+            //  {
+            //      Console.WriteLine("Grade is F");
+            //  }
+
+            //  Ex 6(-------------------6-----------)
+
+
+            //  Console.Write("Input the grade :");
+            // char grd = Convert.ToChar(Console.ReadLine());
+
+            // switch (grd)
+            // {
+            //    case 'E':
+            //         Console.WriteLine(" Excellent");
+            //        break;
+            //    case 'V':
+            //       Console.WriteLine (" Very Good");
+            //       break;
+            //  case 'G':
+            //      Console.WriteLine (" Good ");
+            //       break;
+            //   case 'A':
+            //       Console.WriteLine ("Average");
+            //       break;
+            //  case 'F':
+            //      Console.WriteLine (" Fails");
+            //      break;
+            // default:
+            //     Console.WriteLine("Invalid Grade Found.");
+            //    break;
+            // }
+            // Console.Write("You have chosen:");
+
+            // extern 7(---------------7------------)
+
+            // Console.Write("Input the mounth :");
+            // int mounth =Convert.ToInt32 (Console.ReadLine());
+
+            //switch (mounth)
+            //{
+            //    case 1:
+            //        Console.WriteLine(" January");
+            //       break;
+            //   case 2:
+            //         Console.WriteLine (" February");
+            //        break;
+            //    case 3:
+            //         Console.WriteLine (" March ");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine ("April");
+            //       break;
+            //   case 5:
+            //       Console.WriteLine ("May ");
+            //       break;
+            //   case 6:
+            //       Console.WriteLine("June ");
+            //        break;
+            //   case 7:
+            //       Console.WriteLine(" July");
+            //       break;
+            //   case 8:
+            //       Console.WriteLine("August ");
+            //       break;
+            //  case 9:
+            //       Console.WriteLine("September");
+            //      break;
+            //   case 10:
+            //      Console.WriteLine("October ");
+            ////      break;
+            //  case 11:
+            //      Console.WriteLine("November");
+            //       break;
+            //   case 12:
+            //      Console.WriteLine("December ");
+            //       break;
+            // default:
+            //       Console.WriteLine("Invalid mounth found.");
+            //   break;
+            //   }
+            //   Console.Write("You have chosen:");
+
+
+            //  ex8(--------------8-------------)
+
+
+            // Console.Write("Input the temperature : ");
+            // int tmp= Convert.ToInt32(Console.ReadLine());
+
+            // if (tmp<0)
+            //    Console.Write("Freezing weather");
+            //else if (tmp< 10)
+            //    Console.Write("Very cold weather");
+            //else if (tmp <20)
+            //     Console.Write("Cold weather");
+            // else if (tmp<30)
+            //     Console.Write("Normal in temp");
+            // else if (tmp< 40)
+            //     Console.Write("Its Hot");
+            //  else if (tmp >=40)
+            //    Console.Write("it's very hot");
+            //  else
+            //     Console.Write("Its very hot");
+
+            // ex9 (-------------9-----------)
+
+            Console.WriteLine("Numarul 1");
+            int x = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine("Numarul 2");
+            int y = Convert.ToInt32((Console.ReadLine()));
+
+            int sum = x +y;
+            int Substraction = x - y;
+            int Division = x /y;
+            int Multiplication = x * y;
+            int Modulus = x % y;
+            int Increment = x++;
+            int Decrement = x--;
+            int Result = x+=9;
+            int result = x-=9;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(Substraction);
+            Console.WriteLine(Division);    
+            Console.WriteLine(Multiplication);  
+            Console.WriteLine(Modulus); 
+            Console.WriteLine(Increment);
+            Console.WriteLine(Decrement);
+            Console.WriteLine(Result);
+            Console.WriteLine(result);
 
 
 
-
-
-
-
-
-
-
-
-            }
+        }
 
     }
 }
