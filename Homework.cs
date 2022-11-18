67c4cf0361d42226f0ff311d3c86fd6c8bf78beb
LaurentiuCristian7 <117767249+LaurentiuCristian7@users.noreply.github.com>
2022-11-18 17:18:18 +0200

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Homework
+    {
+        internal void Go()
+        {
+            // ex1
+
+            //Console.WriteLine("Introduceti primul numar:");
+            // int firstNumber = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Introduceti al doilea numar");
+            // int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            // int sum = firstNumber + secondNumber;
+            //int Substraction = firstNumber - secondNumber;
+            //int Division = firstNumber / secondNumber;
+            // int Multiplication = firstNumber * secondNumber;
+
+            // Console.WriteLine(sum);
+            //Console.WriteLine(Substraction);
+            //Console.WriteLine(Division);
+            // Console.WriteLine(Multiplication);
+
+            //ex2( ------------EX2------------)
+            // Console.WriteLine("Input first number:");
+            //  int x = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Input second number:");
+            //  int y = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Input third number:");
+            // int z = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Maximum number: "+Math.Max(x, Math.Max(y, z)));
+            //Console.WriteLine("Minimum number: "+Math.Min(x, Math.Min(y, z)));
+
+            //Console.WriteLine("Enter days:");
+
+            // int x = Convert.ToInt32(Console.ReadLine()); 
+            // int years = (x / 365);
+            // int months = ((x %365)/30);
+            // int days = ((x%365)%30);
+
+            //Console.WriteLine("Years : "+ years);
+            //Console.WriteLine("months : "+months);
+            //Console.WriteLine("Days : "+ days);
+
+            // int z= Convert.ToInt32(Console.ReadLine());
+            // int year = (z / 365);
+            // int month = ((z %365)/30);
+            // int day= ((z%365)%30);
+
+            // Console.WriteLine("Years : "+ year);
+            // Console.WriteLine("months : "+month);
+            // Console.WriteLine("Days : "+ day);
+
+
+
+
+
+
+            //ex 4(---------------------Exercise4----------)
+
+            /// Console.WriteLine("Input first number:");
+            //  int a = Convert.ToInt32(Console.ReadLine());
+            ///// Console.WriteLine("Input second number:");
+            // int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input third number:");
+            // float c = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Maximum number: "+Math.Max(a, Math.Max(b, c)));
+            //Console.WriteLine("Minimum number: "+Math.Min(a, Math.Min(b, c)));
+
+            //ex 5(---------------------Exercice5--------------)
+            
+           // Console.WriteLine("Physics Marks : ");
+           //  int mark1 = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(" Chemistry Marks : ");
+           //  int mark2 = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(" Biology Marks :");
+            // int mark3 = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine("Mathematics Marks");
+           // int mark4= Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(" Computer Science Marks");
+          //   int mark5= Convert.ToInt32(Console.ReadLine());
+
+          //  int total = (mark1 + mark2 + mark3 + mark4 + mark5);
+
+          //  float percentage = total/5f;
+
+
+           // Console.WriteLine("Total Marks : " + total);
+           // Console.WriteLine("Percentage : " + percentage);
+
+            
+           // if (percentage >= 90)
+           // {
+           //     Console.WriteLine("Grade is A");
+          //  }
+          //  else if ( percentage >=80)
+          //  {
+          //      Console.WriteLine("Grade is B");
+          //  }
+           // else if (percentage >= 70)
+          //  {
+          //      Console.WriteLine("Grade is C");
+          //  }
+          //  else if (percentage >= 60)
+           // {
+          //      Console.WriteLine("Grade is D");
+         //   }
+         //   else if (percentage>= 40)
+         //   {
+          //      Console.WriteLine("Grade is E");
+          //  }
+           // else if (percentage <40)
+          //  {
+          //      Console.WriteLine("Grade is F");
+          //  }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+    }
+}
